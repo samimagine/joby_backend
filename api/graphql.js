e83e0c60dcb3627e805f680384d47b0ddb5a54fc
Sam Imagine <sam@samimagine.com>
2024-12-03 14:36:45 +0100

    restart

--- api/graphql.js
+++ api/graphql.js
@@ -2,25 +2,21 @@ const { ApolloServer } = require('@apollo/server');
 const { startServerAndCreateHandler } = require('@as-integrations/vercel');
 const { gql } = require('graphql-tag');
 
-// Define the schema
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `;
 
-// Define resolvers
 const resolvers = {
   Query: {
     hello: () => 'Hello from Apollo Server!',
   },
 };
 
-// Create an Apollo Server instance
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Export the Vercel-compatible handler
 module.exports = startServerAndCreateHandler(server);
